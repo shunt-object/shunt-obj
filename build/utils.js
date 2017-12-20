aec5e7244ca0864fba1ba5cf0b414ec1e27e7c82
wangax <wangaixia@itsmcn.com>
2017-12-20 10:14:23 +0800

    'build修改'

--- build/utils.js
+++ build/utils.js
@@ -92,7 +92,7 @@ exports.createNotifierCallback = function () {
       title: pkg.name,
       message: severity + ': ' + error.name,
       subtitle: filename || '',
-      icon: path.join(__dirname, 'logo.png')
+      //icon: path.join(__dirname, 'logo.png')
     })
   }
 }
