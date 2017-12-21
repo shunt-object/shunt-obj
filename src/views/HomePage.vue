95db9fc8d6dace42291b780aaa7ba6d39a79ac29
Mr Wang <wangyd@itsmcn.com>
2017-12-21 11:39:21 +0800

    王玉东

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="homepage">
+  <div class="homepage" style="background:#f8f8f8;">
      <nav class="navbar navbar-default navbar-fixed-top">
 			<div class="container">
 				<!-- Brand and toggle get grouped for better mobile display -->
@@ -50,27 +50,21 @@
 
      
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width:100%;height:auto">
-            <!-- ol鎸囩ず鍣? ol鏍囩涓巙l鏍囩涓嶅悓 ol涓烘槸鏈夊簭鍒楄〃 ul涓烘槸鏃犲簭鍒楄〃 -->
             <ol class="carousel-indicators">
-                  <!-- 鎸囩ず鍣?-->
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="3"></li>
             </ol>
-
-            <!-- 鍖呰鐨勮疆鎾浘鐗?-->
             <div class="carousel-inner">
-               
-            
                 <div class="item active" style="width:100%;min-width:100%">
                     <div class="lunmain" >
                       <div class="container">
                         <div class="row" style="width:100%">
                           <div class="col-md-7 lun-le">
                               <h1>中国的“RightScale”,助您轻松上云!</h1>
-                              <p class="pCenter">依托国内用户习惯，开发实现了上云的科学化和专业化分析，
-打造了“从工作负载是否可上云?上哪种类型的云服务?如何选择
+                              <p class="pCenter">依托国内用户习惯，开发实现了上云的科学化和专业化分析，</br>
+打造了“从工作负载是否可上云?上哪种类型的云服务?如何选择</br>
 适合云厂商？”一体化线上SaaS服务平台。</p>
                               <p class="pBottom"><span class="pSpan">立即注册</span>&nbsp&nbsp&nbsp<span>了解更多</span></p>
 
@@ -88,8 +82,8 @@
                         <div class="row" style="width:100%">
                           <div class="col-md-7 lun-le">
                               <h1>中国的“RightScale”,助您轻松上云!</h1>
-                              <p class="pCenter">依托国内用户习惯，开发实现了上云的科学化和专业化分析，
-打造了“从工作负载是否可上云?上哪种类型的云服务?如何选择
+                              <p class="pCenter">依托国内用户习惯，开发实现了上云的科学化和专业化分析，</br>
+打造了“从工作负载是否可上云?上哪种类型的云服务?如何选择</br>
 适合云厂商？”一体化线上SaaS服务平台。</p>
                               <p class="pBottom"><span class="pSpan">立即注册</span>&nbsp&nbsp&nbsp<span>了解更多</span></p>
 
@@ -114,22 +108,186 @@
              
             </a>
         </div>
+        <!-------------------------------轮播结束------------------------------------------>
 
-       
+
+      
+      
+        <!--------------------------------公告------------------------------------------------>
         <div class="announce">
-            <div class="row" style="margin-left:60px;">
-                <div class="col-md-3"> <span></span></span>公告信息</div>
-                <div class="col-md-9">
-                    <div class="row">
-                        <span class="col-md-4">哈哈哈哈哈哈哈哈</span>
-                        <span class="col-md-4">哈哈哈哈哈哈哈哈</span>
-                    </div>
-                </div>
+                <ul  class="row col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                  <li class="col-md-3 annou  col-xs-5"><i class="fa fa-bullhorn"></i>公告信息</li>
+                  <li class="col-md-7 col-xs-7 lis" style="text-align:center">
+                    <marquee direction="left">关于未备案网站核查处理公告 11-28
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    CSB再出重磅服务:“智能机器人小易”</marquee>
+                  </li>
+                </ul>
+         </div>
+        <!----------------------------main-start---------------------------------->
+        <div class="obj-main" >
+          <div class="container" style="width:70%">
+            <div class="row text-center" >
+              <h1 class="text-center thin">Saas化云服务</h1>
+              <h3 class="text-center thin">竭力为您提供科学的一站式上云服务</h3>
             </div>
-        </div>
-        <div>
-        </div>
+              <div class="row text-center" style="margin-top:25px">
+                  <div class="col-xs-12 col-sm-6 blurb-cta">
+                      <img src="../assets/yun-f.png" alt="" class="Yunimg">
+                      <h2>云规划</h2>
+                      <h4>我们的工作负载可以上云吗？</h4>
+                      <p class="text-center">通过"云规划最佳实践"，对当前的工作负载进行全面立体分析</br>和科学规划，判断该工作负载是否可上云？精确计算该工作负</br>载云的收益度和云的亲和度，从而得出上哪种类型的云服务。</p>
+                       <span class="obj-cta">查看更多</span>
+
+                  </div>
+                  <div class="col-xs-12 col-sm-6 blurb-cta">
+                       <img src="../assets/yun-s.png" alt="" class="Yunimg">
+                      <h2>云选型</h2>
+                      <h4>我们应该上哪家云呢？</h4>
+                      <p class="text-center">建立在您设计的业务场景之上，通过易云管提供的上百个</br>标准，多维度横向比较和可视化展现公有云服务商所提供</br>的功能，从而为您科学的便捷的快速选择最佳的云厂商。</p>
+                      <span class="obj-cta">查看更多</span>
+                  </div>
+              </div>
+              </div>
+          </div>
+
+         
+          <!---------------------为什么选择我们---------------------------->
+          <div class="obj-what phone-position"  >
+              <img src="../assets/qq.png" alt="" class="RenGong">
+              <img src="../assets/phone.png" alt="" class="phone">
+            <div class="container">
+              <div class="row text-center jzWe" >
+                  <h1 class="text-center thin">为什么选择我们</h1>
+                  <h3 class="text-center thin">云可能很复杂，但我们助您上云变得简单和轻松</h3>
+              </div>
+              <div class="col-xs-12 col-sm-6 obj-what-dv">
+                  <img src="../assets/computer.png" alt="">
+              </div>
+              <div class="col-xs-12 col-sm-6 obj-what-ds">
+                  <h1><img src="../assets/sheng.png" alt="">省力</h1>
+                  <h3>为您减少 50%的上云规划时间</h3>
+                  <p><span>Saas化</span><span>一站式</span><span>智能化</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="obj-what obj-that" >
+            <div class="container" style="background:#fff;width:80%;padding:30px 30px">
+              <div class="col-xs-12 col-sm-6 obj-what-ds obj-what-dc" style="">
+                    <h1><img src="../assets/sheng.png" alt="">专业</h1>
+                    <h3>集结上百位专家心血，&nbsp&nbsp&nbsp</br>为了您提供科学规划上云</h3>
+                    <p class=""><span class="">权威化</span><span class="">科学化</span><span class="">顾问化</span></p>
+                    
+              </div>  
+              <div class="col-xs-12 col-sm-6 obj-what-dv obj-what-da">
+                  <img src="../assets/computer-s.png" alt="">
+              </div>   
+            </div>         
+          </div>
+           <div class="obj-chat" >
+            <div class="container" >
+              <div class="col-xs-12 col-sm-6 obj-what-dv" style="padding:50px 50px">
+                  <img src="../assets/computer.png" alt="">
+              </div>
+              <div class="col-xs-12 col-sm-6 obj-what-ds dsg"> 
+                  <h1><img src="../assets/sheng.png" alt="">省钱</h1>
+                  <h3>洞察您当前的工作负载，&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</br>为您节省 30%的上云成本支出</h3>
+                  <p><span>分析精确化</span><span>结论可观化</span><span>操作工具化</span></p>
+              </div>
+            </div>   
+          </div>
+         
+         
+          <!-----------------------合作伙伴-------------------------------------->
+          <div class=" obj-what obj-friend">
+            <div class="container">
+              <div class="row text-center jzWe jsFei" >
+                  <h1 class="text-center thin">合作伙伴</h1>
+                  <h3 class="text-center thin">诚邀各行业优秀的合作伙伴与我们一起共建生态!</h3>
+              </div>
+              <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                  <img src="../assets/friend.png" alt="">
+              </div>
+               <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                  <img src="../assets/friend.png" alt="">
+              </div>
+               <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                  <img src="../assets/friend.png" alt="">
+              </div>
+               <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                   <img src="../assets/friend.png" alt="">
+              </div>
+               <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                   <img src="../assets/friend.png" alt="">
+              </div>
+               <div class="col-xs-6 col-sm-2 obj-what-dv jzfei">
+                  <img src="../assets/friend.png" alt="">
+                   <img src="../assets/friend.png" alt="">
+              </div>
+        
+              
+            </div>
+          </div>
+
+
+          <!------------------------感言------------------------------------------>
+          <div class=" obj-free ">
+            <div class="container">
+              <div class="col-xs-12 col-sm-12 obj-what-dv jzfei">
+                  <p>
+                    这一路走来，我们经历过坎坷、经历过风雨;</br>
+                    为此，我们曾经彷徨过、迷茫过、哭过、笑过;</br>
+                    但我们从未放弃、倔强而努力;</br>
+                    因为我们坚信:</br>
+                    我们每前进小一步，都将为您带来更好的云上体验!
+                  </p>
+                  <span>立即体验</span>
+              </div>
+            </div>
+          </div>
+
+
+          <!-------------------------footer------------------------------------------>
+               <div class=" obj-friend footer">
+            <div class="container">
+              <div class="col-xs-12 col-sm-3 obj-what-dv footer-ts">
+                  <p class="text-left footer-jx">联系我们</p>
+                  <p class="text-left">400-030-8008</p>
+                  <p class="text-left">1259709119@qq.com</p>
+              </div>
+              <div class="col-xs-12 col-sm-3 obj-what-dv footer-ts">
+                  <p class="text-left footer-jx">功能</p>
+                  <p class="text-left">云规划</p>
+                  <p class="text-left">云选型</p>
+                  <p class="text-left">云管理</p>
+              </div>
+              <div class="col-xs-12 col-sm-3 obj-what-dv footer-ts">
+                  <p class="text-left footer-jx">服务支持</p>
+                  <p class="text-left">帮助中心</p>
+                  <p class="text-left">服务条款</p>
+              </div>
+              <div class="col-xs-12 col-sm-3 obj-what-dv footer-ts footer-left">
+                  <p class="footer-jx">易云管</p>
+                  <div class="footer-ke">
+                      <p>您的专属上云顾问</p>
+                      <img src="../assets/weichat.png" alt="">
+                  </div>
+              </div>
+            </div>
+            <div class="row text-center footer-last">
+                  <p class="text-center thin">© CopyRight 2017江苏京玉信息技术有限公司  版权所有    TEL:(0379）65112858 65112856</p>
+            </div>
+          </div>
+          </div>
+    
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -145,11 +303,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.homepage{
-  width:100%;
+.row{
+  margin-left:0;
 }
 *{
-  font-family:"Microsoft YaHei"
+  font-family:"Microsoft YaHei";
+}
+ul,ol li{
+  list-style:none
 }
 .lunmain {
   background:url('../assets/bg-f.png') no-repeat;
@@ -160,6 +321,18 @@ export default {
 .navbar{
   background:none;
   border:none
+}
+@media (min-width: 768px){
+  .announce ul .lis{
+    overflow: hidden;
+    text-overflow:ellipsis; 
+    white-space: nowrap;
+    height:58px;
+  }
+  .annou{
+    text-align:center;
+  }
+
 }
 @media (min-width: 768px){
   .cl{
@@ -204,7 +377,7 @@ export default {
   margin-top:20px;
 }
 .lunmain .lun-le .pBottom span{
-    padding:5px 15px ;
+    padding:10px 25px ;
     border:1px solid #ccc;
     border-radius:4px;
     color:#fff;
@@ -266,12 +439,262 @@ export default {
 .navbar-nav li span{
   color:#fff;
 }
-.announce{
-  width:100%;
-  height:58px;
-}
+
 
 .announce{
   background:#f2f2f2;
+  line-height:58px;
+  height:58px;
+}
+.annou{
+  font-size:18px;
+  color:#f29f2a;
+  text-align:right
+}
+
+.obj-main{
+  width:100%;
+  
+  background:#ccc
+}
+.obj-main h1{
+  font-size:48px;
+  color:#333333;
+  font-weight:700;
+  font-size:50px;
+  line-height:100px;
+}
+.obj-main h3{
+  font-size:22px;
+  color:#777777;
+   font-weight:100
+}
+.obj-main .Yunimg{
+  width:225px;
+  height:224px;
+}
+.obj-main .obj-cta{
+  padding:5px 20px;
+  border:1px solid #0c72cf;
+  border-radius:4px;
+  color:#0c72cf;
+ 
+  font-size:14px;
+}
+.obj-main p{
+  font-size:14px;
+  color:#666666;
+
+}
+.obj-main h2{
+  font-size:28px;
+  color:#333333;
+}
+.obj-main h4{
+  font-size:18px;
+  color:#333333;
+}
+
+.obj-main .blurb-cta {
+    margin-bottom: 30px;
+}
+.obj-what-dv img{
+  width:100%;
+  height:100%;
+}
+.obj-what{
+  width:100%;
+  margin-bottom:120px;
+}
+.obj-what .container{
+  margin-top:66px;
+  width:80%;
+}
+.obj-what .container .jzWe {
+  margin-bottom:50px;
+}
+.obj-what-ds{
+   margin-top:15px;
+}
+.obj-what-ds h1{
+  font-size:44px;
+}
+.obj-what-ds h1 img{
+  width:45px;
+  height:49px
+}
+.obj-what-ds h3{
+  font-size:28px;
+  color:#666666;
+}
+.obj-what-ds p span{
+  font-size:12px;
+  color:#ffffff;
+  background:#0983f6;
+  border-radius:4px;
+  border:1px solid #0983f6;
+  margin-right:5px;
+}
+.obj-what-ds p{
+  margin-top:30px;
+}
+.obj-what-ds p span:nth-child(1){
+  padding:5px 15px;
+}
+.obj-what-ds p span:nth-child(2){
+  padding:5px 25px;
+}
+.obj-what-ds p span:nth-child(3){
+  padding:5px 15px;
+}
+.obj-what-dc{
+  margin-top:40px;
+}
+.obj-what-da{
+  margin-top:15px;
+}
+.obj-chat{
+  margin-top:50px; 
+}
+.obj-friend{
+  background:#f8f8f8;
+  width:100%;
+  padding-bottom:50px;
+  margin:0;
+  
+}
+.obj-chat,.obj-what h1{
+  color:#333333;
+}
+.jsFei{
+  margin-top:50px;
+}
+.jzfei img{
+  padding-bottom:20px;
+}
+.obj-free{
+  width:100%;
+  background:url("../assets/bg-g.png") no-repeat;
+  background-size:cover;
+  height:auto;
+  padding-bottom:70px;
+}
+.obj-free p{
+  font-size:24px;
+  color:#fafafa;
+  line-height:44px;
+  font-weight:200;
+  padding-top:50px;
+  padding-bottom:30px;
+}
+.obj-free span{
+  border:1px solid #eaa84f;
+  font-size:20px;
+  color:#ffffff;
+  background:#eaa84f;
+  border-radius:4px;
+  padding:8px 50px;
+}
+.obj-free .container{
+  width:75%;
+}
+.footer{
+  background:#000;
+}
+.footer .footer-ts{
+  margin-top:40px;
+}
+.footer p{
+  font-size:12px;
+  color:#b3b3b3;
+}
+.footer-ts img{
+  width:97px;
+  height:97px;
+}
+.footer .container{
+  width:55%;
+}
+.footer .footer-ke{
+  border-left:4px solid #2e383e;
+}
+.footer .footer-jx{
+  font-weight:900
+}
+.footer .footer-last{
+  border-top:4px solid #2e383e;
+  margin-top:20px;
+  line-height:62px;
+}
+.footer .footer-last p{
+  font-size:12px;
+  color:#b3b3b3;
+}
+.footer{
+  padding-bottom:0;
+}
+.dis{
+  padding:50px;
+}
+@media (max-width: 768px){
+  .dsg{
+    padding:0
+  }
+  .obj-chat{
+    margin-top:0;
+  }
+  .obj-what{
+    margin-bottom:0px;
+  }
+  .obj-main .blurb-cta{
+    margin-bottom:50px;
+  }
+  .obj-what-da{
+    margin-bottom: 30px;
+  }
+  .announce ul .lis{
+    overflow: hidden;
+    text-overflow:ellipsis; 
+    white-space: nowrap;
+    height:58px;
+  }
+  .annou{
+    text-align:center;
+  }
+  #bs-example-navbar-collapse-1{
+    background:#000;
+  }
+  .obj-what-ds p span:nth-child(1){
+  padding:5px 0px;
+}
+.obj-what-ds p span:nth-child(2){
+  padding:5px 10px;
+}
+.obj-what-ds p span:nth-child(3){
+  padding:5px 0px;
+}
+}
+
+
+.phone-position{
+  position:relative
+}
+.RenGong{
+  position:absolute;
+  right:0px;
+  top:50%;
+}
+.phone{
+  position:absolute;
+  right:0px;
+  top:70%;
+}
+.RenGong:hover{
+  background:blue;
+  cursor:pointer;
+}
+.phone:hover{
+  background:red;
+  cursor:pointer
 }
 </style>
