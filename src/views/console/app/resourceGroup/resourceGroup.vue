9ccebe3b40beeb729322aec8479fa701a68f0f2f
wangax <wangaixia@itsmcn.com>
2018-01-09 10:51:50 +0800

    '资源配置组样式'

--- src/views/console/app/resourceGroup/resourceGroup.vue
+++ src/views/console/app/resourceGroup/resourceGroup.vue
@@ -1,52 +1,78 @@
 <template>
-    <div>
-           <h1>App servers</h1><input type="text" v-model="num">
-           <ul>
-               <li><input type="number" v-model="cores" >Vcpu</li>
-               <li><input type="text" v-model="ghz">处理器主频</li>
-               <li><input type="text" v-model="ram">内存</li>
-               <li><input type="text" v-model="localDisk">系统盘</li>
-               <li><select name="" id="sele" v-model="os">
-                        <option value="Linux">Linux</option>
-                        <option value="Window">Window</option>
-                        <option value="Unix">Unix</option>
-                   </select>操作系统
-                </li>
-                <li><input type="text" v-model="computeMappingFactor">计算机映射因子</li>
-               <li><input type="text" v-model="monthlyUsage">每个月用量</li>
-               <li><input type="text" v-model="dailyUsage">每天用量</li>
-           </ul>
-                <h1>App SDB Servers</h1><input type="text" v-model="nume">
-                    <ul>
-                        <li><input type="text" v-model="coresq">cpu</li>
-                        <li><input type="text" v-model="ghzq">处理器主频</li>
-                        <li><input type="text" v-model="ramq">内存</li>
-                        <li><input type="text" v-model="localDiskq">系统盘</li>
-                        <li><select name="" id="selet" v-model="osq">
-                                    <option value="Linux">Linux</option>
-                                    <option value="Window">Window</option>
-                                    <option value="Unix">Unix</option>
-                            </select>
-                            </li>
-                            <li><input type="text" v-model="computeMappingFactorq">计算机映射因子</li>
-                        <li><input type="text" v-model="monthlyUsageq">每个月用量</li>
-                        <li><input type="text" v-model="dailyUsageq">每天用量</li>
-                    </ul>
-                <h1>Network</h1>
-                    <ul>
-                        <li><input type="text" v-model="bandwidth ">带宽</li>
-                        <li><input type="text" v-model="inbound">入站</li>
-                        <li><input type="text" v-model="outbound">出站</li>
-                    </ul>
-                <h1>Storage</h1><input type="text" v-model="nums">
-                    <ul>
-                        <li><input type="text" v-model="sna">共享存储</li>
-                        <li><input type="text" v-model="nsa">网络存储</li>
-                        <li><input type="text" v-model="cloudStorage">云存储</li>
-                    </ul>
-
-                   <router-link to="/planQuestion" class="nex"><button style="width:100px;height:50px;background:#ccc" @click="btn()">下一步</button></router-link> 
+<div class="total">
+<div class="total-header">
+    <span></span>
+    <router-link to="/">总览</router-link> > 资源组配置
+</div>
+<div class="plan-box">
+    <div class="plan-container">
+        <child index="2" start="0"></child>
+            <div class="resourceCroup-list row">
+                <div class="resourceCroup-list-head col-md-3">
+                    <span class="padding-65">App servers</span><input type="text" v-model="num">
+                </div>
+                <ul class="resourceCroup-list-ul col-md-9">
+                    <li><input type="number" v-model="cores" >Vcpu</li>
+                    <li><input type="text" v-model="ghz">处理器主频</li>
+                    <li><input type="text" v-model="ram">内存</li>
+                    <li><input type="text" v-model="localDisk">系统盘</li>
+                    <li><select name="" id="sele" v-model="os">
+                                <option value="Linux">Linux</option>
+                                <option value="Window">Window</option>
+                                <option value="Unix">Unix</option>
+                        </select>操作系统
+                        </li>
+                        <li><input type="text" v-model="computeMappingFactor">计算机映射因子</li>
+                    <li><input type="text" v-model="monthlyUsage">每个月用量</li>
+                    <li><input type="text" v-model="dailyUsage">每天用量</li>
+                </ul>
+            </div>
+            <div class="resourceCroup-list row">
+                <div class="resourceCroup-list-head col-md-3">
+                    <span class="padding-65">App SDB Servers</span><input type="text" v-model="nume">
+                </div>
+                <ul class="resourceCroup-list-ul col-md-9">
+                    <li><input type="text" v-model="coresq">cpu</li>
+                    <li><input type="text" v-model="ghzq">处理器主频</li>
+                    <li><input type="text" v-model="ramq">内存</li>
+                    <li><input type="text" v-model="localDiskq">系统盘</li>
+                    <li><select name="" id="selet" v-model="osq">
+                                <option value="Linux">Linux</option>
+                                <option value="Window">Window</option>
+                                <option value="Unix">Unix</option>
+                        </select>
+                        </li>
+                        <li><input type="text" v-model="computeMappingFactorq">计算机映射因子</li>
+                    <li><input type="text" v-model="monthlyUsageq">每个月用量</li>
+                    <li><input type="text" v-model="dailyUsageq">每天用量</li>
+                </ul>
+            </div>
+            <div class="resourceCroup-list row">  
+                <div class="resourceCroup-list-head col-md-3">  
+                    <span class="padding-25">Network</span>
+                </div>
+                <ul class="resourceCroup-list-ul col-md-9">
+                    <li><input type="text" v-model="bandwidth ">带宽</li>
+                    <li><input type="text" v-model="inbound">入站</li>
+                    <li><input type="text" v-model="outbound">出站</li>
+                </ul>
+            </div>
+            <div class="resourceCroup-list row">
+                <div class="resourceCroup-list-head col-md-3">  
+                    <span class="padding-25">Storage</span><input type="text" v-model="nums">
+                </div>
+                <ul class="resourceCroup-list-ul col-md-9">
+                    <li><input type="text" v-model="sna">共享存储</li>
+                    <li><input type="text" v-model="nsa">网络存储</li>
+                    <li><input type="text" v-model="cloudStorage">云存储</li>
+                </ul>
+            </div>
+            <router-link to="/planQuestion" class="nex"><button class="btn btn-default resourcebtn" @click="btn()">下一步</button></router-link> 
+            <div class="clear" style="margin-bottom:20px;"></div>
     </div>
+
+</div>
+</div>
 </template>
 <style>
     .nex{
@@ -57,6 +83,8 @@
     }
 </style>
 <script>
+import child from '../../../../components/steps/steps.vue'
+import '../resourceGroup/resourceGroup.css'
 export default {
   name: 'ResourceGroup',
   data () {
@@ -156,7 +184,10 @@ export default {
             
 
       }}
-  }
+  },
+    components:{
+        child
+    }
 }
   </script>
 
