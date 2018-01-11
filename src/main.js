6d7bcd705d3a63783bb127fb330582b5fdc09891
wangax <wangaixia@itsmcn.com>
2018-01-11 17:18:00 +0800

    'router'

--- src/main.js
+++ src/main.js
@@ -52,21 +52,21 @@ Vue.prototype.logout = function(){
     })
 }
 
-// router.beforeEach((to, from, next) => {
-//     if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
-//         if (sessionStorage.getItem("accountId")) {  // 通过vuex state获取当前的token是否存在
-//             next();
-//         }else {
-//             next({
-//                 path: '/login',
-//                 query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-//             })
-//         }
-//     }
-//     else {
-//         next();
-//     }
-// })
+router.beforeEach((to, from, next) => {
+    if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+        if (sessionStorage.getItem("accountId")) {  // 通过vuex state获取当前的token是否存在
+            next();
+        }else {
+            next({
+                path: '/login',
+                query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+            })
+        }
+    }
+    else {
+        next();
+    }
+})
 
 
 
