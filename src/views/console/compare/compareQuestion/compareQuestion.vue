40466c6c9779d0fe050e43558817c9c37bf22ec6
Mr Wang <wangyd@itsmcn.com>
2018-03-07 18:06:02 +0800

    wud

--- src/views/console/compare/compareQuestion/compareQuestion.vue
+++ src/views/console/compare/compareQuestion/compareQuestion.vue
@@ -241,7 +241,8 @@ export default{
             }
         },
         result:function(){
-            this.$router.push({path:'/design',query:{id:this.appId,type:this.queryType}});
+            // this.$router.push({path:'/design',query:{id:this.appId,type:this.queryType}});
+            this.$router.push({path:'/colligateReport',query:{id:this.appId,type:this.queryType}});
         },
         allSelect:function(e){
             //console.log(this.typelist);
