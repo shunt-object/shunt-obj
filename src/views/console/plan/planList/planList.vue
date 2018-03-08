7bb65e75b0558e857a9af2656b58954b57ee67cf
Mr Wang <wangyd@itsmcn.com>
2018-03-08 15:14:45 +0800

    d

--- src/views/console/plan/planList/planList.vue
+++ src/views/console/plan/planList/planList.vue
@@ -218,6 +218,37 @@ export default {
 
   created: function() {
     this.getData();
+    
+  },
+  updated:function(){
+      $.noConflict();
+           $(document).ready(function(){
+               $("#example").dataTable().fnDestroy();
+                $('#example').DataTable({
+                    "bFilter": true,
+                    "bLengthChange": false,
+                     
+                    
+                    "oLanguage": {
+                        "sSearch": "搜索:",
+                         "sEmptyTable": "没有相关记录",  
+                        "sLengthMenu": "每页显示 _MENU_ 条记录",
+                        "sZeroRecords": "抱歉， 没有找到",
+                        "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+                        "sInfoEmpty": "没有数据",
+                        "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+                        "oPaginate": {
+                            "sFirst": "首页",
+                            "sPrevious": "前一页",
+                            "sNext": "后一页",
+                            "sLast": "尾页"
+                        },
+                        "sZeroRecords": "没有检索到数据",
+                        "sProcessing": "<img src='./loading.gif' />"
+                    }
+                });
+
+            });
   },
   methods: {
     myFun: function() {
@@ -267,6 +298,7 @@ export default {
         },
         err => {}
       );
+     
     },
 
     rems: function() {
