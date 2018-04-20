226b5561fa369cca0467f98a6a0d371d43ff7dac
wangax <wangaixia@itsmcn.com>
2018-04-20 11:16:53 +0800

    '4.20'

--- src/views/console/appCenter/appcenterPlan/appcenterPlan.vue
+++ src/views/console/appCenter/appcenterPlan/appcenterPlan.vue
@@ -1,26 +1,34 @@
 <template>
 <div class="appcenterplan">
-<div class="appcplan-list">
-    <div class="appcplan-item">
-        CPU：<el-input class="appcplan-input" type="number" placeholder="请输入CPU" v-model="matchBo.cores"></el-input>
-    </div>
-    <div class="appcplan-item">
-        内存：<el-input class="appcplan-input" type="number" placeholder="请输入内存" v-model="matchBo.ram"></el-input>
-    </div>
-    <div class="appcplan-item">
-        硬盘：<el-input class="appcplan-input" type="number" placeholder="请输入硬盘" v-model="matchBo.disk"></el-input>
-    </div>
-    <div class="appcplan-btn">
-        <button class="appcplan-button" v-on:click="submit()">完成</button>
-    </div>    
+<div class="appcplan-list"> 
+    <el-form :model="matchBo" :rules="rules" ref="matchBo" label-width="60px">
+        <el-form-item label="CPU" prop="cores" class="appcplan-from-item">
+            <el-input class="appcplan-input" type="number" placeholder="请输入CPU" v-model="matchBo.cores"></el-input>
+        </el-form-item>
+        <el-form-item label="内存" prop="ram" class="appcplan-from-item">
+            <el-input class="appcplan-input" type="number" placeholder="请输入内存" v-model="matchBo.ram"></el-input>
+        </el-form-item>
+        <el-form-item label="硬盘" prop="disk" class="appcplan-from-item">
+            <el-input class="appcplan-input" type="number" placeholder="请输入硬盘" v-model="matchBo.disk"></el-input>
+        </el-form-item>
+        <el-form-item class="appcplan-btn">
+            <el-button type="button" class="appcplan-button" v-on:click="submit('matchBo')">提交</el-button>
+        </el-form-item>
+    </el-form>   
     <div class="appcplan-result" v-if="matchdata!=undefined&&matchdata!=''">
         <div class="appcplan-result-echarts" style="width:100%;height:300px;">
             <div id="appcplan-result-echarts" style="width:100%;height:100%"></div>
         </div>
         <div class="row appcplan-clould-box">
-            <div class="col-md-4 appcplan-clould">云定性：{{matchdata.serviceName}}</div>
-            <div class="col-md-4 appcplan-clould">云亲和度：{{matchdata.affinityScope}}</div>
-            <div class="col-md-4 appcplan-clould">云收益度：{{matchdata.benefitScope}}</div>
+            <div class="col-md-4 appcplan-clould">
+                <div class="appcplan-clould-scoce1"><span>云定性</span>{{matchdata.serviceName}}</div>
+            </div>
+            <div class="col-md-4 appcplan-clould">
+                <div class="appcplan-clould-scoce2"><span>云亲和度</span>{{matchdata.affinityScope}}</div>
+            </div>
+            <div class="col-md-4 appcplan-clould">
+                <div class="appcplan-clould-scoce3"><span>云收益度</span>{{matchdata.benefitScope}}</div>
+            </div>
         </div>
         <div class="appcplan-result-desc" v-html="matchdata.serviceDesc"></div>
     </div>
@@ -44,23 +52,41 @@ export default{
                 ram:'',//内存容量
             },
             matchdata:'',
-            opiniondata:[]
+            opiniondata:[],
+            rules:{
+                ram: [
+                    { required: true, message: '请输入内存容量', trigger: 'blur,change' },
+                ],
+                disk: [
+                    { required: true, message: '请输入硬盘容量', trigger: 'blur,change' },
+                ],
+                cores: [
+                    { required: true, message: '请输入CPU核数', trigger: 'blur,change' },
+                ]
+            }
         }
     },
     methods:{
-        submit:function(){
-            this.$this.post('/broker/match/getConfig',JSON.stringify(this.matchBo)).then((response)=>{
-                //console.log(response.data.data);
-                this.matchdata = response.data.data;
-                this.opiniondata =  [{
-                    name:response.data.data.serviceName,
-                    value:[response.data.data.benefitScope,response.data.data.affinityScope]
-                }] 
-                //console.log(this.opiniondata);
-                this.$nextTick(function() {
-                    this.draw('appcplan-result-echarts')
-                })
-            }).catch((error)=>{})
+        submit:function(matchobj){
+            this.$refs[matchobj].validate((valid) => {
+                if (valid) {
+                    this.$this.post('/broker/match/getConfig',JSON.stringify(this.matchBo)).then((response)=>{
+                        //console.log(response.data.data);
+                        this.matchdata = response.data.data;
+                        this.opiniondata =  [{
+                            name:response.data.data.serviceName,
+                            value:[response.data.data.benefitScope,response.data.data.affinityScope]
+                        }] 
+                        //console.log(this.opiniondata);
+                        this.$nextTick(function() {
+                            this.draw('appcplan-result-echarts')
+                        })
+                    }).catch((error)=>{})
+                } else {
+                    return false;
+                }
+            });
+            
         },
         draw:function(id){
             this.charts = echarts.init(document.getElementById(id));
