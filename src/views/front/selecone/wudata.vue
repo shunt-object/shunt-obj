d5ca5845e2ebbc1513de875e17db76013fe09edd
Mr Wang <wangyd@itsmcn.com>
2018-05-15 17:57:35 +0800

    ,

--- src/views/front/selecone/wudata.vue
+++ src/views/front/selecone/wudata.vue
@@ -189,7 +189,7 @@
             </div>
       
             <div style="background:#ededed;;width:100%;height:auto;">
-                <div class="seleconeMain-footer row">
+                <div class="seleconeMain-footer ">
                     <div class="container text-left" style="width:60%;padding-left:cd30px;">
                             <div class="col-md-3 col-xs-12" style="margin-top:27px;margin-bottom:20px;"><img src="../../../assets/qian.png" alt="" style="width:18%;margin-right:10px"><span style="font-size:14px;color:#333333">5天无理由退款</span></div>
                             <div class="col-md-3 col-xs-12 juzhong " style="margin-top:27px;margin-bottom:20px;"><img src="../../../assets/24fuwu.png" alt="" style="width:18%;margin-right:10px"><span style="font-size:14px;color:#333333">7×24小时顾问支持</span></div>
