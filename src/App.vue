8db94bf604fe88cd8965c64ae6b1afb3e8b70338
Leon <wangleicoder@163.com>
2018-05-23 16:30:26 +0800

    更改友盟https

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,6 @@
     s.parentNode.insertBefore(hm, s);
   })();
            
-
-  var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273069341'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1273069341%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
-//  var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-//       document.write(unescape("%3Cspan id='cnzz_stat_icon_1273069341'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1273069341%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
 export default {
   name: 'app'
 }
